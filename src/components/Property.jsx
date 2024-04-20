0652d2fb843e64c03a777291a0c8774e1a0f41d0
adamsbryan <bryanintech@gmail.com>
2024-04-19 23:01:48 -0700

    Style changes

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -4,16 +4,32 @@ import '../App.scss'
 function Property({address, location, beds, baths, sqft, price}) {
     return(
     <>
+        <canvas className="background-image" />
         <section className='hero'>
             <canvas className="property-image-1" />
         </section>
         <section className="listing-wrapper">
             <div className='listing'>
                 <div className='property-info-wrapper'>
-                    <p>View listing</p>
+                    <div className='property-thumbnail' />
                     <div className='property-info'>
                         <h1>{address}</h1>
                         <h2>{location}</h2>  
+                        <h2 className="price">${price}</h2>
+                        <div className='details'>
+                            <div className='icon'>
+                                <div className='icon-bed' />
+                                <p>{beds} beds</p>
+                            </div>
+                            <div className='icon'>
+                                <div className='icon-bath' />
+                                <p>{baths} baths</p>
+                            </div>
+                            <div className='icon'>
+                                <div className='icon-sqft' />
+                                <p>{sqft} sq. ft.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>    
